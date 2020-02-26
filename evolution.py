9705b7185e509d209b8a2723edd85dd12f519375
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-02-26 20:47:21 +0000

    local files transfered

---
+++ evolution.py
@@ -0,0 +1,24 @@
+FOOD_SIZE = 5
+
+class Population: # Population class
+    def __init__(self):
+        self.added_individuals = 0
+        self.removed_individuals = 0 # number of Individuals removed in total this day
+        self.slaughtered_individuals = 0 # number of Individuals killed by other Individuals this day
+        self.food_eaten = 0
+
+    def setupPopulation(self, population):
+        self.population = population # list of Individuals in the Population
+
+class Individual: # Individual class
+    def __init__(self, population, size, location, vision_distance, eating_distance):
+        self.population = population # the Population the Individual belongs to
+        self.size = size
+        self.location = location
+        self.vision_distance = vision_distance
+        self.eating_distance = eating_distance
+
+class Food:
+    def __init__(self, location, nutrition):
+        self.location = location
+        self.nutrition = nutrition
